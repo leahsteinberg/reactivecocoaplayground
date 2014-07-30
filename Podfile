5ecaf87c4a5e7c519d5897bd893fbc33369534ec
Leah Steinberg <leah.steinberg@venmo.com>
2014-07-30 18:23:59 -0400

    first commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "6.0"
+
+target "playground" do
+
+pod 'ReactiveCocoa', '2.1.4'
+
+end
--- playground/FRPViewController.m
+++ playground/FRPViewController.m
@@ -1,14 +1,11 @@
-//
-//  FRPViewController.m
-//  playground
-//
-//  Created by Leah Steinberg on 7/30/14.
-//  Copyright (c) 2014 LeahSteinberg. All rights reserved.
-//
+
 
 #import "FRPViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface FRPViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *createAccountButton;
 
 @end
 
@@ -17,6 +14,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    RACSignal *validEmailSignal = [self.textField.rac_textSignal map:^id(NSString * value){
+        return @([value rangeOfString:@"@"].location != NSNotFound);
+    }];
+    
+    RAC(self.createAccountButton, enabled) = validEmailSignal;
+    
+    RAC(self.textField, textColor) = [validEmailSignal map:^id(id value){
+        if ([value boolValue]){
+            return [UIColor greenColor];
+        }
+        else {
+            return [UIColor redColor];
+            
+        }
+    }];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
